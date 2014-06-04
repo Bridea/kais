89db67191864b5cb74fc59d253fa3e35608999e0
orion <orion@orion-PC>
2014-06-04 17:38:02 +0700

    commit transkrip

--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -133,20 +133,20 @@ jQuery(document).ready(function(e) {
 	/*-----------------------------------------------------------------------------------*/	
     $(function() {
     	$('a#scroll-top').show();
-//    	 $(window).scroll(function () {
-//             if(!$('body').hasClass('probably-mobile'))
-//             {
-//                 if ($(this).scrollTop() > 50) {
-//                     $('a#scroll-top').fadeIn();
-//                 } else {
-//                     $('a#scroll-top').fadeOut();
-//                 }
-//             }
-//             else
-//             {
-//                 $('a#scroll-top').fadeOut();
-//             }
-//         });
+    	 $(window).scroll(function () {
+             if(!$('body').hasClass('probably-mobile'))
+             {
+                 if ($(this).scrollTop() > 50) {
+                     $('a#scroll-top').fadeIn();
+                 } else {
+                     $('a#scroll-top').fadeOut();
+                 }
+             }
+             else
+             {
+                 $('a#scroll-top').fadeOut();
+             }
+         });
 
         $('a#scroll-top').on('click', function(){
             if(!$('body').hasClass('probably-mobile'))
