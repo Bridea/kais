b8883c7a3fa489dd5772594ef3ce2bd900fa9338
WahyudinScatt <Wahyudin.1040132@gmail.com>
2014-06-06 16:46:43 +0700

    STRESS TESTING

--- src/main/java/com/bridea/siak/bean/LoginBean.java
+++ src/main/java/com/bridea/siak/bean/LoginBean.java
@@ -102,7 +102,7 @@ public class LoginBean extends DialogBean {
 			visit.setCurrentPemiih(login);
 			System.out.println("Visit ==> " + visit);
 			UtilBean.setValueExpression("#{sessionScope.visit}", visit);
-			redirect("/staff.akademik.jsf?faces-redirect=true");
+			redirect("/staff.akademik.jsf");
 		} else {
 			System.out.println("Check Data False");
 		}
@@ -126,7 +126,7 @@ public class LoginBean extends DialogBean {
 		if (session != null) {
 			session.invalidate();
 		}
-		redirect("/login.jsf?faces-redirect=true");
+		redirect("/login.jsf");
 	}
 
 }
