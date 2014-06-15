9dfbecb2e4fe89d05a09fdd048f6899e824b344e
WahyudinScatt <Wahyudin.1040132@gmail.com>
2014-06-15 18:24:23 +0700

    Beres

--- src/main/java/com/bridea/siak/bean/RekapNilaiBean.java
+++ src/main/java/com/bridea/siak/bean/RekapNilaiBean.java
@@ -1,6 +1,6 @@
 package com.bridea.siak.bean;
 
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.bridea.siak.model.MKomponenNilai;
+import com.bridea.siak.util.JSFMessageUtil;
 
 import net.sf.jasperreports.engine.JRDataSource;
 
@@ -122,6 +123,11 @@ public class RekapNilaiBean extends AbstractBaseReportBean {
 							.equals(getTempWaktu())) {
 				listKomponenNilais.add(mKomponenNilai);
 			}
+		}
+		if (listKomponenNilais.size() < 0) {
+			JSFMessageUtil messageUtil = new JSFMessageUtil();
+			messageUtil
+					.sendInfoMessageToUser("Data Nilai Yang Anda Cari Tidak Tersedia");
 		}
 		System.out.println(listKomponenNilais.size());
 	}
